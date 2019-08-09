cb1b1e6e4e0024d58d3fdc598cc6546441e3ff54
yatharthb97 <yatharth1997@gmail.com>
2019-08-09 15:02:51 +0530

    RenderingEngine File

--- RenderingEngine.cpp
+++ RenderingEngine.cpp
@@ -1,1 +1,22 @@
-class RenderingEngine{};
+#pragma once
+#include<ostream>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+	ofstream orend;
+	orend.open("~/MCS/Render/initialrender.dat");
+	if(orend.is_open)
+	{
+		std::cout<<"File intialrender.dat :: open succesfull."<<endl;
+	}
+
+	else
+	cerr<<"File initialrender.dat ::open failure!"<<endl;
+void createxyz(&c_position[])
+{
+	orend<<setprecision(7)<<c_position[0]<<"\t"<<c_position[1]<<"\t"<<c_position[2]<<endl;
+}
+
+
+
