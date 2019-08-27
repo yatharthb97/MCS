7bf958269d325395f9361476d6aaff237d6a659f
yatharthb97 <yatharth1997@gmail.com>
2019-08-27 23:32:45 +0530

    Major updates

--- log.h
+++ log.h
@@ -1,1 +1,56 @@
-class log{};
+#include<iostream>
+#include<fstream>
+
+#include "./Global.h"
+
+
+ofstream eo;
+ofstream oo;
+eo.open(errorlog, ios::in);
+oo.open(outputlog, ios::in);
+
+if(RUN == false){eo.close(); oo.close(); }
+
+
+class Log{
+
+public:
+
+	void logerror(const char* filename, string arguement){
+		std::cerr<<"[ERROR] "<<filename<<"  ==>"<<arguement<<std::endl;
+
+		if(eo.is_open())
+			eo<<"[ERROR] "<<filename<<"  ==>"<<arguement<<std::endl;
+		else{
+			std::cerr<<"[FATAL ERROR] log failure: logerror"<<std::endl;
+			exit(1);
+		}
+
+
+	void logout(const char* filename, string arguement, bool out){
+
+		if(out == true){
+			std::cerr<<<<filename<<"  ==>"<<arguement<<std::endl;
+
+			if(oo.is_open())
+			oo<<"[LOG] "<<filename<<"  ==>"<<arguement<<std::endl;
+			else{
+			Log::logerror(outputlog, "[FATAL ERROR] log failure: logerror");
+			}
+		}
+
+		else{
+
+			if(oo.is_open())
+			oo<<"[LOG] "<<filename<<"  ==>"<<arguement<<std::endl;
+			else{
+			Log::logerror(outputlog, "[FATAL ERROR] log failure: logerror");
+			}
+
+		}23
+	}
+
+
+}
+
+};//end of class Log
--- particle.h
+++ particle.h
@@ -1,70 +1,148 @@
-//Particle Header class
-//Comments incomplete
+//Header file for Particle class
+
+//Periodic boundary conditions not implemented
 
 #pragma once
+
+#include "./Vector.h"
 
 class particle
 {
 public:
-	//Constructor - only for energy
-	particle();
+/////Constructor - only for energy
 	//Initializes energy
-
-	//Destructor
-	~particle();
-
-	//Overloading Initializer
-	particle(&double[], &double[]);
-		//Calls initializer
-		//Calls initializer and sets up the initial environment
-		//@param - &position[], &orientation[]
-
-	//Translaton Updator - Mutator
-	void translator(&double[], &double[]);
-		//Updates the position of the particle
-		//@param &c_position[], &translate[]
-
-	//Orientation Updator - Mutator
-	void orienter(&double[], &double[]);
-		//Updates the angular orientation of the particle
-		//@param &orient[], &c_orientation[]
-//-------------------------------------------------------------------Comments incomplete after this line
-	//Energy Updator - Mutator
-	void energiser(&double, &double[]);
-		//Updates the energy of the particle
+	particle()
+	{
+		energy=0.000000; //Float precision - 6
+		c_energy=0.000000; //Float precision - 6
+	}
 	
+
+/////Destructor
+	~particle(){}
+
+/////Overloading Constructor
+	//@brief - Calls initializer and sets up the initial environment
+	//@param - partid ==> particle ID	
+	particle(int partid)
+	{
+		//random::initializerP(); //Notdefined - class random
+		//random::initializerO();	//Notdefined - class random
+		position.x, position.y, position.z = random::initializerP();
+		orientation.a, orientation.b, orientation.c, orientation.d = random::initializerO();
+		this->partid = partid;
+		//Returns 6 values of randomly generated coordinates
+		Render::createxyz(filename); //INsert filename config reference
+			//Creates a trajectory file of the initial setup*/
+	}
+
+//////Accessor Functions
+
 	//getPosition - Accessor
-	double getPosition(&c_position[]) const;
-		//Returns the position array of the particle
-		//@return double c_position[]
-	
-	void printPosition(&c_position[]) const;
-		//Prints the position array of the particle And ends line
-		//@param - c_position[]
+	//@brief - Returns the position array of the particle
+	//@return - position[]
+	double getPosition() const
+	{
+		
+		return(this->position);
+	}
 
 	//getOrientation - Accessor
-	double getOrientation(&c_orientation) const;
-		//Returns the orientation of the particle
-		//@return - double c_orientation[]
-
-	void printOrientation(&c_orientation) const;
-		//Prints the position array of the particle and ends line
-		//@param - &c_orientation[]
+	//@brief - Returns the orientation of the particle
+	//@return - orientation[]
+	double getOrientation() const
+	{
+		
+		return(this->orientation);
+	
+	}
 
 	//getEnergy - Accessor
-	double getEnergy(&c_energy) const;
-		//Returns the Energy of the particle
-		//@return - c_energy
+	//@brief - Returns the Energy of the particle And ends line
+	//@return - double energy
+	double getEnergy() const
+	{
+		
+		return(this -> energy);
+	}
 
 
+	//getc_Energy - Accessor
+	//@brief - Returns the Chemical Energy of the particle And ends line
+	//@return - double c_energy
+	double getc_Energy() const
+	{
+		
+		return(this -> c_energy);
+	}
 
+
+/////Mutator Functions
+
+	//Translaton Updator - Mutator
+	//@brief - Updates the position of the particle
+	//	   Contains rejection condition, periodic boundary conditions not implemented
+	//@param - double &translate[]
+	void translator(double &translate[])
+	{
+		if((position.x+=translate[0] && position.y+=translate[1] && < position.z+=translate[2]) < EDGE)
+		//if(for(i = 0 && 1 && 2):(c_position[0]+=c_translate[0]) < EDGE)
+		{
+			
+				position.x+=translate[0];
+				position.y+=translate[1];
+				position.z+=translate[2];
+		}
+		else
+			{
+				Log::logerror([Move Rejected],"Tranlation move out of bounds!");
+				REJECT = REJECT+1; //REJECT defined globally
+			}
+	}
+
+	//Orientation Updator - Mutator
+	//brief - Updates the angular orientation of the particle
+	// 	  Contains no rejection condition
+	//@param - double &orient[]
+	void orienter(double &orient[])
+	{
+		
+			orientation.a+=orient[0];
+			orientation.b+=orient[1];
+			orientation.c+=orient[2];
+			orientation.d+=orient[3];
+	}
+
+	//Energy Updator - Mutator
+	//@brief - Updates the energy of the particle
+	//	   Contains no rejecion condition
+	//@param - double energise
+	void energiser(double &energise)
+	{
+		energy = energise;
+	}
+
+	//Chemical Energy Updator - Mutator
+	//@brief - Updates the cheical energy of the particle
+	//	   Contains no rejecion condition
+	//@param - double energise
+	void energiser(double &c_energise)
+	{
+		energy+=energise;
+	}
+
+	
 private:
-	//Member variables
-	double c_position[3];
-	double c_orientation[3];
-	double c_energy;
-	//double c_translate[3];
-	//double c_orient[3];
+/////Member variables
+	V.position;
+	Q.orientation;
+	double energy;
+	double c_energy
+	int partid;
 
+
+
+/////Friend declarations
+Friend class Render;
 
 };
