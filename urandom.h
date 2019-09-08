42621b9f22aae49ea89369f3031715cbdc3221b3
Yatharth <yatharth1997@gmail.com>
2019-09-08 22:37:23 +0530

    class defined for urandom

--- urandom.h
+++ urandom.h
@@ -1,14 +1,27 @@
 //C code for urandom ==> seed by system entropy ///Include licence information
+
+
+//Preprocessors
 #pragma once
-
-
 #include<unistd.h>
 #include<fcntl.h>
 
+
+//class Urandom
+class Urandom{
+
+private: 
+
+int seed;
+
+public:
 int seedby_urandom(){
-	int ur, fd;
-	fd = open("/dev/urandom", O_RDONLY);
+	int ur, fd, seed;
+	fd = open("/dev/urandom", O_RDONLY, O_NONBLOCK); //Non-blocking mode implemented
 	read(fd,&ur,sizeof(int));
 	close(fd);
-	return (ur>0)?ur:-ur;
+	seed = (ur>0)?ur:-ur;
+	return seed;
 }
+
+};//End of class Urandom
