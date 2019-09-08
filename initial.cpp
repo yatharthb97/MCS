9fcf6750d8d7bdebdecc1403e2839b023b40f372
yatharthb97 <52491002+yatharthb97@users.noreply.github.com>
2019-09-08 22:39:05 +0530

    Initializer Functions defined

---
+++ initial.cpp
@@ -0,0 +1,48 @@
+//Implementation for Initializer
+
+//Dependencied ==> urandom.h for seedby_urandom()
+//			   ==> Global.cpp for checkBoxSize()
+
+
+#include<random>
+#include<iostream>
+#include "urandom.h" //seedby_urandom
+#include <cstdlib> //using rand
+#include <ctime> //using time
+//#include<log.h> //Log errors
+using namespace std;
+
+
+extern volatile checkBoxSize();
+void initializerP(&double, &double, &double);
+void initializerO(&double, &double, &double, &double);
+
+
+
+void initializerP(&double x, &double y, &double z)
+{
+	double BoxSize = checkBoxSize();
+	ranlux48 rlx; // ranlux48_base rlx; //Creating object for RANLUX random number generator
+	uniform_real_distribution<double> drandom(0.0, BoxSize);
+
+	Urandom urandom;//Object for Urandom class
+	int sx = urandom.seedby_urandom(); //Seeding
+	rlx.seed(sx);
+	std::srand(std::time(nullptr)); // use current time as seed for random generator
+
+	x = drandom(rlx);
+	int sy = sx+std::rand();
+	rlx.seed(sy);
+	y = drandom(rlx);
+
+	int sz = sx+std::rand();
+	rlx.seed(sz);
+	z = drandom(rlx);
+
+}
+
+
+void initializerO(&double a, &double b, &double c, &double d)
+{
+	///
+}
