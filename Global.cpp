b5a0e151f37c5f85ea9ecb9d41b505191199c99c
Yatharth <yatharth1997@gmail.com>
2019-09-15 02:36:18 +0530

    Major changes made

--- Global.cpp
+++ Global.cpp
@@ -5,14 +5,19 @@ define bool RUN = true;*/
 
 //fileconfig
 #define REJECT
+#define ACCEPT
 REJECT = 0;
+ACCEPT = 0;
+
+//1
 //Switch to indicate if the program is running
-bool volatile checkRUN()
+inline bool volatile checkRUN()
 {
 	return true;
 }
 
-volatile const char* fileConfig(int i)
+//2
+inline volatile const char* fileConfig(int i)
 {
 	switch(i)
 	{	
@@ -22,19 +27,21 @@ volatile const char* fileConfig(int i)
 	}
 }
 
-
-volatile int checkStepSize()
+//3
+inline volatile int checkStepSize()
 {
 	return(1); //STEP SIZE
 }
 
-volatile double checkBoxSize()
-{
-	return(); //Size of the box
-}
-
-volatile void Reject(int a)
+//4
+inline volatile void Reject(int a)
 {
 	if(a==1) {extern REJECT+=1;}
+}
+
+//5
+inline accept void Accept(int a)
+{
+	if(a==1){extern ACCEPT+=1; }
 }
 
