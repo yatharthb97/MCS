87b9bd1aa5d84a5fbb251ae46fc9192df8ef69c8
Yatharth <yatharth1997@gmail.com>
2019-09-15 23:55:16 +0530

    LJLoop function moved here and modified

--- particle_energy_calc.h
+++ particle_energy_calc.h
@@ -1,37 +1,41 @@
 //Particle Engergy Calculator - Calculate the energy terms of the particle using hamiltonian and reject the move if the energy calculated is out of the box
+#include<cmath>
+
+//Truncated LJ Potential - Weeks-Chandler Anderson Potential
 
 
-//Truncated LJ Potential - Need a box energy
-
-double LjLoop()
-{
-	double Eta, sigma ;
+	double Eta = 0.0000; //Set value
+	double sigma = 0.0000; //Set value
 	double A = 4*Eta*pow(sigma,12);
 	double B = 4*Eta*pow(sigma,6);
-	double rc = 1000000; //Cutoff distance
+	double rc = 2.5*sigma; //Cutoff distance of 2.5 Sigma
+	double C= (A/pow(rc,6));
+	double D = B/(rc6*rc6);
+	double vlj = 0.0000; //Initialize
+
+
+double LjLoop(std::vector<Particle> &list) //Skeleton Function
+{
+	//Neither shifted nor truncated
+	V r; //Define vector r
 	for(unsigned int i = 1; i<=count; i++)
 	{
 		for(unsigned int j = 1; j<=count; j++)
 		{
 			if(i!=j) //If i = j, the contribution is zero. 
 			{
-				r = b.partlist(i).size() - b.partlist(j).size();
-				if(r>rc) //Accept only if the distance
+				r = b.partlist(i).getPosition() - b.partlist(j).getPosition();
+				r = r.size();
+				if(r<=rc) //Accept only if the distance is less than cutoff distance
 				{
 					r6 = pow(r,6);
 					r12 = r6*r6;
-					E+= (A/r12) - (B/r6);
+					Vlj+= ((A/r12) - (B/r6)) -(C-D);
 				}
 			}	
 		}
 	}
 
-	return E;
-}
-
-
-if(Ej < Ei)
-{
-
+	return Vlj;
 }
 
