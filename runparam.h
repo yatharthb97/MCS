c012dbb54a818f6fb1d06be66696cca9e0b93676
Yatharth <yatharth1997@gmail.com>
2019-10-05 05:08:13 -0700

    New Class Created

---
+++ runparam.h
@@ -0,0 +1,55 @@
+#pragma once
+
+class RunParam{
+
+public:
+//Member VAriables
+static bool RUN;
+static double StepSize;
+static int LJARatio;
+static double BoxSize;
+static const* char LogOutput;
+static const* char LogErrorOut;
+
+
+
+//1
+//Accessor - checkRUN()
+//Switch to indicate if the program is running
+bool volatile checkRUN()
+{
+	return RUN;
+}
+
+//2
+//Accessor - checkStepSize()
+//Returns the max StepSize
+volatile double checkStepSize()
+{
+	return StepSize;
+}
+
+//3
+volatile int checkLJARatio()
+{
+	return checkLJARatio; //Acceptance of higher energy configuration
+}
+
+//4
+volatile double checkBoxSize()
+{
+	return BoxSize;
+}
+//5
+volatile const char* fileConfig(int i)
+{
+	switch(i)
+	{	
+		//Log Files:: demanded by class Log
+		case 1:	return(LogErrorOut); break; //For error log 
+		case 2: return(LogOutput); break; //For output log
+	}
+}
+
+};
+
