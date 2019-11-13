18ff27cc29bf244e23fbda290180de6b24b1c909
Yatharth <yatharth1997@gmail.com>
2019-11-13 21:49:56 +0530

    Added Partial Energy Calculation Function

--- particle_energy_calc.cpp
+++ particle_energy_calc.cpp
@@ -106,3 +106,85 @@ double LjLoop(std::vector<Particle> &partlist)
 
 }
 
+double PartEnergyCalc(std::vector<Particle> &partlist, int i) 
+{
+	//Probably Not Shifted
+	double Vlj = 0.000000; //Initializes
+	V r; //Define vector r
+	int count = partlist.size();
+
+		for(unsigned int j = 0; j<count; j++)
+		{
+			if((i!=j) && (partlist.at(i).isGhost() == false) && (partlist.at(j).isGhost()) == false)//If i = j, the contribution is zero and if particle is ghost.
+			{
+				
+				r = partlist.at(i).getPosition() - partlist.at(j).getPosition();
+				double rval = r.size();
+
+				//Dynamic Positiona CutOFF
+				V point = partlist.at(i).position.point(partlist.at(j).position);
+
+				if(point.arccos(partlist.at(i).patch.unit) > PI/4)
+				{
+					if(rval<=(Particle::L+Particle::D/2))
+					{
+						return 999;
+					}
+				}
+
+				else
+				{
+					if(rval<=Particle::D)
+					{
+						return 999;
+					}
+				}
+
+				point = point*-1;
+				if(point.arccos(partlist.at(j).patch.unit) > PI/4)
+				{
+					if(rval<=(Particle::L+Particle::D/2))
+					{
+						return 999;
+					}
+				}
+
+				else
+				{
+					if(rval<=Particle::D)
+					{
+						return 999;
+					}
+				}
+				//Dynamic Positional CutOFF
+				
+
+				//Repulsion part - only position dependent
+				//Repulsion Portion
+				if(rval<=CutOffRep) //Accept only if the distance is less than cutoff distance
+				{
+					double r6 = pow(rval,6);
+					double r12 = r6*r6;
+					Vlj+= ((A/r12) - (B/r6)) -(C-D); 
+				}
+				//End of Repulsion part - LJLoop
+
+				//Attraction Portion
+				double normo = partlist.at(i).patch.normal.dot(partlist.at(j).patch.normal);
+				double unito = partlist.at(i).patch.unit.dot(partlist.at(j).patch.unit);
+
+				if (rval <= 1.5*SigmaAtrr)
+				{
+					if(acos(normo) <= (Patch::width)/2 && acos(normo) >= -(Patch::width)/2)
+					{
+						Vlj+= -EtaAtrr*unito*unito - EtaAtrr*2; //Last Term: -EtaAtrr*SigmaAtrr/Radius ==> diameter = SigmaAtrr
+					}
+				}
+			}	
+		}
+	
+
+	//cout<<"Energy: "<<Vlj<<endl;
+	return Vlj;
+}
+
--- particle_energy_calc.h
+++ particle_energy_calc.h
@@ -1,4 +1,5 @@
 //Particle Engergy Calculator - Calculate the energy terms of the particle using hamiltonian and reject the move if the energy calculated is out of the box
+#pragma once
 #include<cmath>
 #include<vector>
 #include"runparam.h"
@@ -20,6 +21,7 @@
 	//double rc = 2.5*SigmaRep; //Cutoff distance of 2.5 Sigma
 	double C= (A/pow(CutOffRep,6));
 	double D = B/(CutOffRep*CutOffRep);
+
 
 
 double LjLoop(std::vector<Particle> &partlist) 
@@ -106,3 +108,85 @@ double LjLoop(std::vector<Particle> &partlist)
 
 }
 
+double PartEnergyCalc(std::vector<Particle> &partlist, int i) 
+{
+	//Probably Not Shifted
+	double Vlj = 0.000000; //Initializes
+	V r; //Define vector r
+	int count = partlist.size();
+
+		for(unsigned int j = 0; j<count; j++)
+		{
+			if((i!=j) && (partlist.at(i).isGhost() == false) && (partlist.at(j).isGhost()) == false)//If i = j, the contribution is zero and if particle is ghost.
+			{
+				
+				r = partlist.at(i).getPosition() - partlist.at(j).getPosition();
+				double rval = r.size();
+
+				//Dynamic Positiona CutOFF
+				V point = partlist.at(i).position.point(partlist.at(j).position);
+
+				if(point.arccos(partlist.at(i).patch.unit) > PI/4)
+				{
+					if(rval<=(Particle::L+Particle::D/2))
+					{
+						return 999;
+					}
+				}
+
+				else
+				{
+					if(rval<=Particle::D)
+					{
+						return 999;
+					}
+				}
+
+				point = point*-1;
+				if(point.arccos(partlist.at(j).patch.unit) > PI/4)
+				{
+					if(rval<=(Particle::L+Particle::D/2))
+					{
+						return 999;
+					}
+				}
+
+				else
+				{
+					if(rval<=Particle::D)
+					{
+						return 999;
+					}
+				}
+				//Dynamic Positional CutOFF
+				
+
+				//Repulsion part - only position dependent
+				//Repulsion Portion
+				if(rval<=CutOffRep) //Accept only if the distance is less than cutoff distance
+				{
+					double r6 = pow(rval,6);
+					double r12 = r6*r6;
+					Vlj+= ((A/r12) - (B/r6)) -(C-D); 
+				}
+				//End of Repulsion part - LJLoop
+
+				//Attraction Portion
+				double normo = partlist.at(i).patch.normal.dot(partlist.at(j).patch.normal);
+				double unito = partlist.at(i).patch.unit.dot(partlist.at(j).patch.unit);
+
+				if (rval <= 1.5*SigmaAtrr)
+				{
+					if(acos(normo) <= (Patch::width)/2 && acos(normo) >= -(Patch::width)/2)
+					{
+						Vlj+= -EtaAtrr*unito*unito - EtaAtrr*2; //Last Term: -EtaAtrr*SigmaAtrr/Radius ==> diameter = SigmaAtrr
+					}
+				}
+			}	
+		}
+	
+
+	//cout<<"Energy: "<<Vlj<<endl;
+	return Vlj;
+
+}
